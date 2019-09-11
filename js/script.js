740a6055ad5ed8e53b9793c8138e755fa477ea60
Mikkel Andersen <normannmail@gmail.com>
2019-09-11 11:33:44 +0200

    progressbaren fungerer og har fået blå farve

---
+++ js/script.js
@@ -0,0 +1,15 @@
+
+// progressbar oppe i toppen
+document.addEventListener(
+  "scroll",
+  function() {
+    var scrollTop =
+      document.documentElement["scrollTop"] || document.body["scrollTop"];
+    var scrollBottom =
+      (document.documentElement["scrollHeight"] ||
+        document.body["scrollHeight"]) - document.documentElement.clientHeight;
+    scrollPercent = scrollTop / scrollBottom * 100 + "%";
+    document.getElementById("progress").style.setProperty("--scroll", scrollPercent);
+  },
+  { passive: true }
+);
