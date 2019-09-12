cb6449987242dc485494e236c58e1e313fa84f83
Mikkel Andersen <normannmail@gmail.com>
2019-09-12 14:32:38 +0200

    kan skifte fra et billede til et andet

--- js/script.js
+++ js/script.js
@@ -18,3 +18,19 @@ document.addEventListener(
 var vid = document.getElementById("reelVideo");
 vid.autoplay = true;
 vid.load();
+
+
+var pic = document.getElementById('bil')
+var x = 1;
+function changeImage() {
+  if (x=1) {
+    pic.style.backgroundImage = 'url("../Images/coffee.png")';
+    x=3;
+
+  }
+  else {
+    pic.style.backgroundImage = 'url("../Images/weed.png")';
+    x = 1;
+
+  }
+}
